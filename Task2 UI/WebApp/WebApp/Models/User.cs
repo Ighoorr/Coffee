25ca7bcf5682c1934d5b3063de2e53dfbcda10ca
Ighoorr <i.bereshchak@gmail.com>
2023-06-16 15:05:47 +0300

    ui task

---
+++ Task2 UI/WebApp/WebApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApp.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int? Age { get; set; }
+        public string Email { get; set; }
+    }
+}
